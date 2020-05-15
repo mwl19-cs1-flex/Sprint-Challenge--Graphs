411c4d42f781910b0e3c55144305ae458f7e0b11
MWeberLambdaweb19 <mack.webb37@gmail.com>
2020-05-15 00:46:30 -0700

    BFT works

--- adv.py
+++ adv.py
@@ -60,13 +60,59 @@ traversal_path = []
 visited_path = set()
 st = Stack()
 qt = Queue()
-print(room_graph)
-print('current amount of rooms in map', len(room_graph))
-print(f'Current rooms visited: {len(visited_path)}')
-print(player.current_room.get_exits())
-for i in player.current_room.get_exits():
-    direction = i.join('_to')
-    print('join',direction)
+
+st.push(player.current_room)
+qt.enqueue(player.current_room)
+print(world.starting_room)
+
+# while visited_path != room_graph:
+#     cell = qt.dequeue()
+#     print(cell)
+#     if cell not in visited_path:
+#         visited_path.add(cell)
+#     for i in cell.get_exits():
+#         next_cell = cell.get_room_in_direction(i)
+#         if next_cell not in visited_path:
+#             traversal_path.append(i)
+#             qt.enqueue(next_cell)
+
+while st.size() > 0:
+    cell = st.pop()
+    if cell not in visited_path:
+        visited_path.add(cell)
+    for i in cell.get_exits():
+        next_cell = cell.get_room_in_direction(i)
+        if next_cell not in visited_path:
+            traversal_path.append(i)
+            st.push(next_cell)
+
+# print(room_graph)
+# print('current amount of rooms in map', len(room_graph))
+# print(f'Current rooms visited: {len(visited_path)}')
+# print(player.current_room.get_exits())
+# for i in player.current_room.get_exits():
+#     print(f'current room id:{player.current_room.id}')
+#     print(f'next rooms: {player.current_room.get_room_in_direction(i)}')
+
+# Understand
+# Current room is an instance of a Room class which has
+# # ID
+# # Room in direction 
+# # Possible exits of that room
+
+# Plan
+# Have a stack
+# Append the first room to the stack
+# While the stack is not zero
+# # Take off the item of the stack
+# # Add the room id to the visited path set
+# # For each exit in the rooms
+# # Get the room in the direction
+# # If that room's id is NOT in visited
+# # # Add the room to the stack
+# # # Add the direction (i) to the traversal path
+
+# Legacy Code
 # while room_graph != visited_path:
 #     for i in player.current_room.get_exits():
 #         player.travel(i)
@@ -75,7 +121,7 @@ for i in player.current_room.get_exits():
 #         if player.travel(i) == "You cannot move in that direction.":
 #             pass
 
-
+# NOTES FROM BEEJ
 # Walk in a stack (go backwards!)
 # DFT then BFT (look at the hints)
 # Get NEIGHBORS!!!
@@ -83,9 +129,7 @@ for i in player.current_room.get_exits():
 # While loop of DFT
 # Then loop of BFT
 # Understanding and Planning is HUGE part of this!!!
-
 # This is the Travelling Salesman problem
-
 # Learning framework Beej uses to be better teacher: UPER! 
 
 # TRAVERSAL TEST - DO NOT MODIFY
@@ -103,7 +147,8 @@ else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
-
+for i in visited_rooms:
+    print(i.id)
 
 #######
 # UNCOMMENT TO WALK AROUND
